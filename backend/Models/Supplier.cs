860b9fd7b6ba9148118c22fdec8c71da6217c89d
Zoya <zdydo@xbsoftware.by>
2025-05-11 20:47:50 +0200

    [add] Shopify Auth

---
+++ backend/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactName { get; set; }
+        public string Contact {  get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Currency { get; set; }
+        public DateOnly WorkStart { get; set; }
+        public bool isVATPayer { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>( );
+    }
+}
